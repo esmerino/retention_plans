92d85c7aedf065370f179fae72b19a8519d5a79f
José Esmerino <jose.esmerino@gmail.com>
2021-03-21 12:47:51 -0300

    Define logic to check plan retention by time.

--- lib/retention_plans.rb
+++ lib/retention_plans.rb
@@ -1,8 +1,39 @@
 # frozen_string_literal: true
 
 require_relative "retention_plans/version"
+require 'date'
 
 module RetentionPlans
   class Error < StandardError; end
-  # Your code goes here...
+
+  class << self
+    def retaine?(date:, plan:)
+      rule_plan(date, plan)
+    end
+
+    private
+
+    def rule_plan(date, plan)
+     return standard_rule(date) if plan == 'standard'
+     return gold_rule(date) if plan == 'gold'
+     return platinum_rule(date) if plan == 'platinum'
+
+     raise Error
+    end
+
+    def standard_rule(date)
+      date = DateTime.parse(date)
+      DateTime.now > date.next_day(42)
+    end
+
+    def gold_rule(date)
+      date = DateTime.parse(date)
+      DateTime.now > date.next_day(42).next_month(12)
+    end
+
+    def platinum_rule(date)
+      date = DateTime.parse(date)
+      DateTime.now >  date.next_day(42).next_month(12).next_year(7)
+    end
+  end
 end
--- spec/retention_plans_spec.rb
+++ spec/retention_plans_spec.rb
@@ -5,7 +5,104 @@ RSpec.describe RetentionPlans do
     expect(RetentionPlans::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '#retaine?' do
+    before do
+      allow(DateTime).to receive(:now).and_return(current_date)
+    end
+
+    context 'when standard plan' do
+      let(:plan) { 'standard' }
+
+      context 'when retaine true' do
+        let(:current_date) { DateTime.parse('20210321113021').next_day(42) }
+
+        it do
+          date = '20210320113021'
+          expect(described_class.retaine?(date: date, plan: plan)).to be_truthy
+        end
+      end 
+
+      context 'when retaine false' do
+        let(:current_date) { DateTime.parse('20210321113021') }
+
+        it do
+          date = '20210322113021'
+          expect(described_class.retaine?(date: date, plan: plan)).to be_falsy
+        end
+      end
+    end
+
+    context 'when gold plan' do
+      let(:plan) { 'gold' }
+
+      context 'when retaine true' do
+        let(:current_date) do 
+          DateTime.parse('20210321113021')
+                  .next_day(42)
+                  .next_month(12)
+        end
+
+        it do
+          date = DateTime.parse('20210320113021').strftime('%Y%m%d%H%M%S')
+
+          expect(described_class.retaine?(date: date, plan: plan)).to be_truthy
+        end
+      end 
+
+      context 'when retaine false' do
+        let(:current_date) do 
+          DateTime.parse('20210321113021')
+                  .next_day(42)
+                  .next_month(12)
+        end
+
+        it do
+          date = DateTime.parse('20210320113021')
+                         .next_day(43)
+                         .next_month(13)
+                         .strftime('%Y%m%d%H%M%S')
+
+          expect(described_class.retaine?(date: date, plan: plan)).to be_falsy
+        end
+      end
+    end
+
+    context 'when platinum plan' do
+      let(:plan) { 'platinum' }
+
+      context 'when retaine true' do
+        let(:current_date) do 
+          DateTime.parse('20210321113021')
+                  .next_day(42)
+                  .next_month(12)
+                  .next_year(8)
+        end
+
+        it do
+          date = DateTime.parse('20210320113021').strftime('%Y%m%d%H%M%S')
+
+          expect(described_class.retaine?(date: date, plan: plan)).to be_truthy
+        end
+      end 
+
+      context 'when retaine false' do
+        let(:current_date) do 
+          DateTime.parse('20210321113021')
+                  .next_day(42)
+                  .next_month(12)
+                  .next_year(7)
+        end
+
+        it do
+          date = DateTime.parse('20210320113021')
+                         .next_day(42)
+                         .next_month(11)
+                         .next_year(6)
+                         .strftime('%Y%m%d%H%M%S')
+
+          expect(described_class.retaine?(date: date, plan: plan)).to be_falsy
+        end
+      end
+    end
   end
 end
